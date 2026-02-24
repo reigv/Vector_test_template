d664a6ef39412ffe94efcac451117bd3025ec809
Son1017 <reig.vaeo.soiv099@gmail.com>
2026-02-24 18:26:18 -0500

    1.2

--- vector.cpp
+++ vector.cpp
@@ -11,12 +11,26 @@ class Vector {
         Vector() {}
         Vector(T x_, T y_): vec_x(x_), vec_y(y_) {}
         
-        Vector operator+(const Vector &other) const {
+        Vector operator+ (const Vector &other) const {
             return Vector(vec_x + other.vec_x, vec_y + other.vec_y);
+        }
+
+        Vector operator+ (T scalar) const {
+            return Vector(scalar + vec_x, scalar +vec_y);
+        }
+
+        friend Vector operator+ (T scalar, const Vector& v) {
+            return Vector(v.vec_x + scalar, v.vec_y + scalar);
         }
 
         void print() const{
             std::cout << "x and y\n" << vec_x << ", " << vec_y << std::endl;
+        }
+
+        // improve print
+        friend std::ostream& operator<< (std::ostream& os, const Vector& v) {
+            os << "(" << v.vec_x << ", " << v.vec_y << ")";
+            return os;
         }
 };
 
@@ -32,5 +46,15 @@ int main () {
 
     std::cout << "v4 = ";
     v4.print();
+
+    std::cout << "v3: " << v3 << std::endl;
+
+    Vector<int> v5 = v2 + 2;
+
+    std::cout << "v5: " << v5 << std::endl;
+
+    Vector<int> v6 = 2 + v2;
+
+    std::cout << "v6: " << v6 << std::endl;
 
 }
